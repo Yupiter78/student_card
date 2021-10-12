07008eb9ab24575c52d0d6906de70e817cc91a34
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-12 23:34:19 +0300

    student-card created components__3

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./components/card";
+import Edit from "./components/edit";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>App</h1>
+            <Card />
+            <Edit />
+        </div>
+    );
+}
+
+export default App;
